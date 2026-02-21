10a7f5d908213868e9d921702292fdc2efa341d8
minhtungmobile-maker <minhtungmobile@gmail.com>
2026-02-22 01:33:05 +0700

    Add files via upload

---
+++ page/index.js
@@ -0,0 +1,16 @@
+﻿// pages/index.js
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>Mứt Gừng Minh Anh</h1>
+      <p>Landing page chuẩn HSTAI 777 đã sẵn sàng!</p>
+      <ul>
+        <li>Liền mạch = 1</li>
+        <li>Rủi ro = 0</li>
+        <li>Không phân mảnh</li>
+        <li>Auto full nguồn lực môi trường riêng</li>
+      </ul>
+      <p>Đặt hàng ngay để trải nghiệm sản phẩm chất lượng cao.</p>
+    </main>
+  );
+}
